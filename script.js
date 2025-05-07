737dbc77ece6fa9baa2a5f86bc4a2b19e0c7c91d
kavish-website <khannavinay1313@gmail.com>
2025-05-07 20:56:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+// Basic form submit handler
 document.getElementById("resellerForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("Thank you for your interest! We'll get back to you soon.");
+  alert("Thank you for your interest! We'll get back to you shortly.");
   this.reset();
 });
