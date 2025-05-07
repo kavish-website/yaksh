fc39da3a801ce3aded1a9d4063a321ee0f239488
kavish-website <khannavinay1313@gmail.com>
2025-05-07 20:31:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("resellerForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for your interest! We'll get back to you soon.");
+  this.reset();
+});
